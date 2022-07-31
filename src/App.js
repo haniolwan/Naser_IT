c321c7b193e2d67d95c59e51ed3f0168627d5983
HaniOlwan <heenoow@gmail.com>
2022-08-01 00:06:52 +0300

    About Us Created Relates #5

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
 import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AboutUs from './components/About Us';
 function App() {
   return (
-    <Header />
+    <>
+      <Header />
+      <AboutUs />
+    </>
   );
 }
 
---
+++ src/components/About Us/index.jsx
@@ -0,0 +1,25 @@
+import { Container, Row, Col } from 'react-bootstrap';
+import aboutImg from '../../assets/aboutImg.png';
+import './style.css';
+const AboutUs = () => {
+    return (
+        <Container className="mt-5">
+            <Row className="content">
+                <Col className="right-content">
+                    <img src={aboutImg} class="img-fluid about-image" alt="AboutUs.jpg" />
+                </Col>
+                <Col className="">
+                    <h2 className="about-text">About us</h2>
+                    <p className="about-description">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec consequat felis.
+                    </p>
+                    <p className="about-description2">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec consequat felis.
+                    </p>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default AboutUs;
