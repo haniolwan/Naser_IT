22842055190327953fd085035236fb4dad9aa254
HaniOlwan <heenoow@gmail.com>
2022-08-03 15:05:25 +0300

    Footer Created Relates #16

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -4,5 +4,14 @@ import Services from './Services';
 import Contact from './Contact';
 import OurClients from './Our Clients';
 import Tools from './Tools';
+import Footer from './Footer';
 
-export { Header, AboutUs, Services, Contact, OurClients, Tools }
+export {
+    Header,
+    AboutUs,
+    Services,
+    Contact,
+    OurClients,
+    Tools,
+    Footer
+}
