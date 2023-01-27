bf118cddefe205a7edf205318b593050f0dc1ebb
Hani Olwan <49692805+haniolwan@users.noreply.github.com>
2023-01-28 01:59:24 +0200

    Add Empty Env Keys

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -13,9 +13,9 @@ import contactImg from '../../assets/contact-img.jpg';
 import './style.css';
 
 const { Control, Select } = Form;
-const serviceId = process.env.REACT_APP_SERVICE_ID;
-const templateId = process.env.REACT_APP_TEMPLATE_ID;
-const publicKey = process.env.REACT_APP_PUBLIC_KEY;
+const serviceId = '' || process.env.REACT_APP_SERVICE_ID;
+const templateId = '' || process.env.REACT_APP_TEMPLATE_ID;
+const publicKey = '' || process.env.REACT_APP_PUBLIC_KEY;
 
 function Contact() {
   const form = useRef();
