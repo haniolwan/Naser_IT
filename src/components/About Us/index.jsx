98a708569b556f2170e7858961236bfcfa05106b
Hani Olwan <heenoow@gmail.com>
2023-09-22 14:18:26 +0300

    About us text change;

--- src/components/About Us/index.jsx
+++ src/components/About Us/index.jsx
@@ -1,11 +1,10 @@
-import { Container, Row, Col } from 'react-bootstrap';
-import { LazyLoadImage } from 'react-lazy-load-image-component';
-import img from '../../assets/Header/aboutImg.webp';
-import './style.css';
+import { Container, Row, Col } from "react-bootstrap";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import img from "../../assets/Header/aboutImg.webp";
+import "./style.css";
 
 function AboutUs() {
   return (
-
     <Container className="mt-5">
       <Row className="about-us-content">
         <Col className="right-content">
@@ -18,11 +17,13 @@ function AboutUs() {
         </Col>
         <Col className="about-us-left-content">
           <h2 className="about-text">About us</h2>
-          <p className="about-description">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec consequat felis.
-          </p>
           <p className="about-description2">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec consequat felis.
+            Seasoned QA Expert: With a strong focus on functional safety, ISO
+            standards, and test automation, I ensure the reliability of
+            automotive systems. Proven problem-solver, I'm your go-to
+            professional for safeguarding automotive technology. Collaborate
+            with confidence - effective communication and expertise in QA and
+            testing make me an asset for your automotive projects.
           </p>
         </Col>
       </Row>
